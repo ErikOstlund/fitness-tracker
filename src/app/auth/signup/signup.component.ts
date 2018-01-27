d6a4a2992f33d69926be4e9c7ec4d4ac6053aaea
Erik Ostlund <ostlu058@umn.edu>
2018-01-27 17:48:14 -0600

    31 Restricting pickable dates
    Must be 18 years old

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -2,19 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+    selector: 'app-signup',
+    templateUrl: './signup.component.html',
+    styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+    maxDate: Date;
 
-  ngOnInit() {
-  }
+    constructor() { }
 
-  onSubmit(form: NgForm) {
-      console.log(form);
-  }
+    ngOnInit() {
+        this.maxDate = new Date();
+        this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
+    }
+
+    onSubmit(form: NgForm) {
+        console.log(form);
+    }
 
 }
