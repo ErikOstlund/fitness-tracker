eadc8be380f35ab00e8f8b37061c41ec0283fc59
Erik Ostlund <ostlu058@umn.edu>
2018-01-30 10:54:48 -0600

    42 Created header and sidenav-list components
    …inside a new ‘navigation’ folder.
    Then ripped out the templates for each from the app.component and placed in proper compoment.
    Then emitted events from components to the app.component to open/close the side nav.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,8 @@ import { PastTrainingComponent } from './training/past-training/past-training.co
 import { WelcomeComponent } from './welcome/welcome.component';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './navigation/header/header.component';
+import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
 
 @NgModule({
   imports: [
@@ -34,7 +36,9 @@ import { AppRoutingModule } from './app-routing.module';
     CurrentTrainingComponent,
     NewTrainingComponent,
     PastTrainingComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    HeaderComponent,
+    SidenavListComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
