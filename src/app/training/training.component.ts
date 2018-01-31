01c8ada6111e5aac8090b1d9dd2b54c41d6840b0
Erik Ostlund <ostlu058@umn.edu>
2018-01-31 17:39:53 -0600

    44 Added tabs to Training page

--- src/app/training/training.component.ts
+++ src/app/training/training.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-training',
-  templateUrl: './training.component.html',
-  styleUrls: ['./training.component.css']
+    selector: 'app-training',
+    templateUrl: './training.component.html',
+    styleUrls: ['./training.component.css']
 })
 export class TrainingComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
