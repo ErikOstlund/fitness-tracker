43aa5c8c84bec909d57fe9cfe49fa914bc5b7395
Erik Ostlund <ostlu058@umn.edu>
2018-02-01 10:05:24 -0600

    47 Added Progress Spinner
    and now hiding the new exercise card if an exercise is started.

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-current-training',
-  templateUrl: './current-training.component.html',
-  styleUrls: ['./current-training.component.css']
+    selector: 'app-current-training',
+    templateUrl: './current-training.component.html',
+    styleUrls: ['./current-training.component.css']
 })
 export class CurrentTrainingComponent implements OnInit {
 
-  constructor() { }
+    progress = 0;
 
-  ngOnInit() {
-  }
+    constructor() { }
+
+    ngOnInit() {
+        setInterval(() => {
+            this.progress = this.progress + 5;
+        }, 1000);
+    }
 
 }
